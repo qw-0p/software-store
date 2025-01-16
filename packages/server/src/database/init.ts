207721c90e226f111a13dc9b357f67b426bf752d
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-01-16 14:08:39 +0200

    chore: added alter option for dev/test env

--- packages/server/src/database/init.ts
+++ packages/server/src/database/init.ts
@@ -10,26 +10,29 @@ import {
   Rating,
 } from './models';
 
+const isDev = process.env.NODE_ENV === 'development';
+const isTest = process.env.NODE_ENV !== 'test';
+
 const init = async () => {
   try {
     console.time();
-    await User.sync();
+    await User.sync({ alter: isDev || isTest });
     console.log('===> USERS table synchronized successfully. <===');
-    await Company.sync();
+    await Company.sync({ alter: isDev || isTest });
     console.log('===> COMPANIES table synchronized successfully. <===');
-    await Category.sync();
+    await Category.sync({ alter: isDev || isTest });
     console.log('===> CATEGORY table synchronized successfully. <===');
-    await Product.sync();
+    await Product.sync({ alter: isDev || isTest });
     console.log('===> PRODUCTS table synchronized successfully. <===');
-    await Basket.sync();
+    await Basket.sync({ alter: isDev || isTest });
     console.log('===> BASKETS table synchronized successfully. <===');
-    await TypeProduct.sync();
+    await TypeProduct.sync({ alter: isDev || isTest });
     console.log('===> TYPE_PRODUCT table synchronized successfully. <===');
-    await BasketProduct.sync();
+    await BasketProduct.sync({ alter: isDev || isTest });
     console.log('===> BASKET_PRODUCT table synchronized successfully. <===');
-    await ProductInfo.sync();
+    await ProductInfo.sync({ alter: isDev || isTest });
     console.log('===> PRODUCT_INFO table synchronized successfully. <===');
-    await Rating.sync();
+    await Rating.sync({ alter: isDev || isTest });
     console.log('===> RATING table synchronized successfully. <===');
     console.log('All tables synchronized successfully.');
     console.timeEnd();
