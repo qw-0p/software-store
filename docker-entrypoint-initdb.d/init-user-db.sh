09c01c400ac5494356c0e955493cbd2977c7d037
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-02-03 12:58:08 +0200

    deleted unecessary file

--- docker-entrypoint-initdb.d/init-user-db.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER postgres;
-	CREATE DATABASE software_store_dev;
-	GRANT ALL PRIVILEGES ON DATABASE software_store_dev TO postgres;
-EOSQL
